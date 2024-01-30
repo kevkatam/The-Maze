6725daef4e9b7d58f25f06ea5db6f4d337b1b5ca
kevkatam <kevkatam@gmail.com>
2024-01-30 09:48:46 +0000

    modified maze.h

--- headers/maze.h
+++ headers/maze.h
@@ -1,10 +1,19 @@
 #ifndef MAZE_H
 #define MAZE_H
 
+#define ScreenWidth 640
+#define ScreenHeight 480
+#define map_w 10
+#define map_h 10
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
 #include <SDL2/SDL.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /**
 * struct SDL_Instance - structure for an instance of SDL
@@ -18,6 +27,14 @@ typedef struct SDL_Instance
 	SDL_Window *wind;
 	SDL_Renderer *rend;
 } SDL_t;
+
+int init_window(SDL_t *init);
+int getmap(int w, int h, int mt);
+void setmap(int w, int h, int value);
+void makemap(char **argv);
+
+
+
 
 
 
