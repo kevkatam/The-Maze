835880ae929ac6f714b54b640bea6163667e33ad
kevkatam <kevkatam@gmail.com>
2024-01-30 09:47:12 +0000

    adding map.c

---
+++ src/map.c
@@ -0,0 +1,67 @@
+#include "../headers/maze.h"
+
+int map[map_w][map_h] = {
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 1, 1, 1, 1, 1, 1, 0, 1},
+	{1, 0, 1, 0, 0, 0, 0, 1, 0, 1},
+	{1, 0, 1, 0, 1, 1, 0, 1, 0, 1},
+	{1, 0, 1, 0, 1, 1, 0, 1, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+};
+
+int floor_map[map_w][map_h] = {
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+	{0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+	{0, 1, 0, 1, 1, 1, 1, 0, 1, 0},
+	{0, 1, 0, 1, 0, 0, 1, 0, 1, 0},
+	{0, 1, 0, 1, 0, 0, 1, 0, 1, 1},
+	{0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+};
+
+/**
+ * getmap - finds map value at given coordinat
+ * @w: x-coordinat
+ * @h: y-coordinate
+ * @mt: map type
+ * Return: value at w, h
+ */
+int getmap(int w, int h, int mt)
+{
+	if (mt == 1)
+		return (floor_map[h][w]);
+	return (map[h][w]);
+}
+/**
+ * setmap - sets value of map at a given coordinate
+ * @w: x-coordinate
+ * @h: y-coordinate
+ * @value: the value to be set
+ * Return: nothing
+ */
+void setmap(int w, int h, int value)
+{
+	map[h][w] = value;
+}
+/**
+ * makemap - makes map from given file
+ * @argv: command line argument array
+ * Return: nothing
+ */
+void makemap(char **argv)
+{
+	int i, j;
+	int **filemap;
+
+	filemap = getaltitude(argv);
+
+	for (i = 0; i < map_w; i++)
+	{
+		for (j = 0; j < map_h; j++)
+			map[i][j] = filemap[i][j];
+	}
+	free_map(filemap);
+}
--- src/window.c
+++ src/window.c
@@ -8,14 +8,25 @@ int init_window(SDL_t *init)
 {
 	if (SDL_Init(SDL_INIT_VIDEO) != 0)
 	{
-		fprint(stderr, "Unable to initialize SDL: %s\n",
+		fprintf(stderr, "Unable to initialize SDL: %s\n",
 		       SDL_GetError());
 		return (1);
 	}
-	init->wind = SDL_CreateWindow("The Maze", 0, 0, SCREEN_WIDTH,
-			SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
+	init->wind = SDL_CreateWindow("The Maze", 0, 0, ScreenWidth,
+			ScreenHeight, SDL_WINDOW_OPENGL);
 	if (init->wind == NULL)
 	{
-		fprint(stderr, "SDL_CreateWindow ERROR: %s\n", SDL_GetError());
+		fprintf(stderr, "SDL_CreateWindow ERROR: %s\n", SDL_GetError());
 		return (1);
 	}
+
+	init->rend = SDL_CreateRenderer(init->wind, -1,
+			SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (init->rend == NULL)
+	{
+		fprintf(stderr, "SDL_CreateRenderer Error: %s\n",
+				SDL_GetError());
+		SDL_Quit();
+		return (1);
+	}
+}
