380492761c184a061ee6658cfd98074f947c919f
kevkatam <kevkatam@gmail.com>
2024-02-21 17:47:16 +0300

    adding comments to code

--- src/weapon.c
+++ src/weapon.c
@@ -6,26 +6,33 @@
  */
 void drawweapon(SDL_t init)
 {
+	/** sdl2 variables, width and height **/
 	SDL_Texture *tx = NULL;
 	SDL_Rect shooter;
 	int w, h;
 	SDL_Surface *s = IMG_Load("images/weapon.png");
 
+	/** if image is present **/
 	if (s)
 	{
+		/** remove background white color **/
 		SDL_SetColorKey(s, SDL_TRUE, SDL_MapRGB(s->format, 255, 255, 255));
+		/** create texture **/
 		tx = SDL_CreateTextureFromSurface(init.rend, s);
+		/** query tx to find width and height of image **/
 		SDL_QueryTexture(tx, NULL, NULL, &w, &h);
 		shooter.w = w * WeaponScale;
 		shooter.h = h * WeaponScale;
 		shooter.x = (ScreenWidth - shooter.w) / 2;
 		shooter.y = (ScreenHeight - shooter.h) + 20;
 
+		/** draw the weapon with above features **/
 		if (tx)
 			SDL_RenderCopy(init.rend, tx, NULL, &shooter);
 		else
 			printf("failed to create texture\n");
-
+		
+		/** free and destroy surface **/
 		SDL_FreeSurface(s);
 		SDL_DestroyTexture(tx);
 	}
@@ -48,37 +55,46 @@ void drawenemy(SDL_t init)
 	int w, h, i;
 	float dx, dy, ds, sx, DOF, ea, ed, es, ex;
 
+	/** for each enemy **/
 	for (i = 0; i < NumEnemy; i++)
 	{
+		/** load the enemy image **/
 		SDL_Surface *s = IMG_Load(enemies[i].p);
 
+		/** if image is present **/
 		if (s)
 		{
+			/** remove background color **/
 			SDL_SetColorKey(s, SDL_TRUE, SDL_MapRGB(s->format, 255, 255, 255));
 			tx = SDL_CreateTextureFromSurface(init.rend, s);
 			SDL_QueryTexture(tx, NULL, NULL, &w, &h);
-
+			
+			/** distance between player and enemy **/
 			dx = enemies[i].x - gamer.x;
 			dy = enemies[i].y - gamer.y;
 			ds = sqrt(dx * dx + dy * dy);
-
+			/** angle and enemy distance relative to the player **/
 			ea = atan2(dy, dx) - gamer.a;
 			ed = cos(ea) * ds;
 
+			/** get depth of field **/
 			DOF = fdv();
 			es = map_size * DOF / ed;
 			sx = tan(ea) * DOF;
 
+			/** get screen position of the enemy **/
 			enm.x = (ScreenWidth / 2) + sx - (es / 2);
 			ex = ed / 4;
 			enm.y = (ScreenHeight - es) / 2.0f;
 			enm.w = es - 200;
 			enm.h = es - 20;
+			/** if enemy is not far from the player **/
 			if (tx && ex > 0 && ex < numrays && enm.y < buf[(int)ex])
 				SDL_RenderCopy(init.rend, tx, NULL, &enm);
 		}
 		else
 			printf("image not found\n");
+		/** free and destroy surface **/
 		SDL_FreeSurface(s);
 		SDL_DestroyTexture(tx);
 	}
@@ -89,6 +105,7 @@ void drawenemy(SDL_t init)
  */
 float fdv(void)
 {
+	/** variables **/
 	float s = ScreenWidth / 2, a = FOV / 2;
 
 	return (s / tan(a));
@@ -100,18 +117,22 @@ float fdv(void)
  */
 void enemymap(SDL_t init)
 {
+	/** variables for SDL_Rect instance and enemy corrdinates **/
 	SDL_Rect rec;
 	float ex, ey;
 	int i;
 
+	/** for each enemy **/
 	for (i = 0; i < NumEnemy; i++)
 	{
+		/** normalize enemy position relative to the map **/
 		ex = enemies[i].x * SCALE;
 		ey = enemies[i].y * SCALE;
 		rec.x = ex - 2;
 		rec.y = ey - 2;
 		rec.w = 5;
 		rec.h = 5;
+		/** draw the map **/
 
 		SDL_SetRenderDrawColor(init.rend, 0, 255, 255, 0);
 		SDL_RenderFillRect(init.rend, &rec);
--- src/window.c
+++ src/window.c
@@ -6,22 +6,26 @@
 */
 int init_window(SDL_t *init)
 {
+	/** if window fails to initialize **/
 	if (SDL_Init(SDL_INIT_VIDEO) != 0)
 	{
 		fprintf(stderr, "Unable to initialize SDL: %s\n",
 		       SDL_GetError());
 		return (1);
 	}
+	/** create the window with given width and height **/
 	init->wind = SDL_CreateWindow("The Maze", 0, 0, ScreenWidth,
 			ScreenHeight, SDL_WINDOW_OPENGL);
+	/** if window is not created **/
 	if (init->wind == NULL)
 	{
 		fprintf(stderr, "SDL_CreateWindow ERROR: %s\n", SDL_GetError());
 		return (1);
 	}
-
+	/** create the renderer **/
 	init->rend = SDL_CreateRenderer(init->wind, -1,
 			SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	/** if renderer fails to be created **/
 	if (init->rend == NULL)
 	{
 		fprintf(stderr, "SDL_CreateRenderer Error: %s\n",
@@ -29,6 +33,7 @@ int init_window(SDL_t *init)
 		SDL_Quit();
 		return (1);
 	}
+	/** if sdl2 image is not initialized **/
 	if (IMG_Init(IMG_INIT_PNG) != IMG_INIT_PNG)
 	{
 		fprintf(stderr, "Failed to initialize SDL_image.\n");
@@ -43,8 +48,10 @@ int init_window(SDL_t *init)
  */
 float fix_angle(float angle)
 {
+	/** if angle is beyond 360 degrees **/
 	if (angle > 2 * M_PI)
 		angle -= 2 * M_PI;
+	/** if angle is less than 0 **/
 	if (angle < 0)
 		angle += 2 * M_PI;
 	return (angle);
